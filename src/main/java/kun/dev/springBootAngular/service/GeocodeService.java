9cdc4fa4184182c91c2631626fe7a33c7332bba2
kun <kun.zou@gmail.com>
2020-01-03 00:10:08 -0600

    use heroku env

--- src/main/java/kun/dev/springBootAngular/service/GeocodeService.java
+++ src/main/java/kun/dev/springBootAngular/service/GeocodeService.java
@@ -13,7 +13,7 @@ public class GeocodeService {
     LatLng location = new LatLng();
 
     GeoApiContext context = new GeoApiContext.Builder()
-        .apiKey("AIzaSyBQtADp6_iewgAvgtmRu3YTx7eLHXxYQvQ")
+        .apiKey(System.getenv("GMAP_KEY"))
         .build();
     try {
       GeocodingResult[] results = GeocodingApi.newRequest(context).address(address + "Winnipeg, MB").await();
